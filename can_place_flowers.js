c7175f4671f594c898427c6b5c1541b0042f0803
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2025-01-05 09:01:33 +0600

    605. Can Place Flowers solved

---
+++ can_place_flowers.js
@@ -0,0 +1,19 @@
+var canPlaceFlowers = function (flowerbed, n) {
+    let count = 0;
+    for (let i = 0; i < flowerbed.length; i++) {
+        if (flowerbed[i] === 0) {
+            let prev = i === 0 || flowerbed[i - 1] === 0;
+            let next = i === flowerbed.length - 1 || flowerbed[i + 1] === 0;
+            if (prev && next) {
+                flowerbed[i] = 1;
+                count++;
+                if (count >= n) return true;
+            }
+        }
+    }
+    return count >= n;
+};
+
+let flowerbed = [1, 0, 0, 0, 1], n = 1;
+console.log(canPlaceFlowers(flowerbed, n));
+
