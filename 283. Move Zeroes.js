61b0d54afd5614da36d1330034c77ece5217a9d5
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2025-02-11 22:36:32 +0600

    nonzeros solved

---
+++ 283. Move Zeroes.js
@@ -0,0 +1,35 @@
+// 283. Move Zeroes
+//
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+//
+// Note that you must do this in-place without making a copy of the array.
+//
+//
+//
+// Example 1:
+//
+// Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+// Example 2:
+//
+// Input: nums = [0]
+// Output: [0]
+
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let nonZero = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            [nums[nonZero],nums[i]] = [nums[i], nums[nonZero]];
+            nonZero++;
+        }
+    }
+    return nums;
+};
+
+nums = [2,0,0,3,12]
+console.log(moveZeroes(nums))
